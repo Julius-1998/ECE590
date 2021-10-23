9e5de39044a6f06b4e919e59b60722bc7f1eecc9
Julius-1998 <89859253+Julius-1998@users.noreply.github.com>
2021-10-23 15:02:18 -0400

    BubbleSort uncompleted

--- Project1/project1.py
+++ Project1/project1.py
@@ -28,6 +28,16 @@ def SelectionSort(listToSort):
 InsertionSort
 """
 def InsertionSort(listToSort):
+    for i in range(len(listToSort)):
+        num = listToSort[i]
+        j = i-1
+        while j >=0 and listToSort[j]>listToSort[j+1]:
+            listToSort[j+1],listToSort[j] = listToSort[j],listToSort[j+1]
+            j = j-1
+        pass
+        listToSort[j+1] = num
+    pass
+
     return listToSort
 
 """
@@ -40,9 +50,36 @@ def BubbleSort(listToSort):
 MergeSort
 """
 def MergeSort(listToSort):
+    if len(listToSort) <= 1:
+        return
+    else:
+        m = len(listToSort)//2
+        l = listToSort[:m]
+        r = listToSort[m:]
+        MergeSort(l)
+        MergeSort(r)
+        lPivot = 0
+        rPivot = 0
+        ansPivot = 0
+        while lPivot < len(l) and rPivot < len(r):
+            if l[lPivot] < r[rPivot]:
+                listToSort[ansPivot] = l[lPivot]
+                lPivot = lPivot + 1
+            else:
+                listToSort[ansPivot] = r[rPivot]
+                rPivot = rPivot + 1
+            ansPivot = ansPivot + 1
+        while lPivot < len(l):
+            listToSort[ansPivot] = l[lPivot]
+            lPivot = lPivot + 1
+            ansPivot = ansPivot + 1
+        while rPivot < len(r):
+            listToSort[ansPivot] = r[rPivot]
+            rPivot = rPivot + 1
+            ansPivot = ansPivot + 1
     return listToSort
-
 """
+
 QuickSort
 
 Sort a list with the call QuickSort(listToSort),
@@ -52,8 +89,23 @@ def QuickSort(listToSort, i=0, j=None):
     # Set default value for j if None.
     if j == None:
         j = len(listToSort)
+    if j-i<=1:
+        return listToSort
+    s = partition(listToSort,i,j)
+    QuickSort(listToSort,i,s)
+    QuickSort(listToSort,s+1,j)
     return listToSort
 
+def partition(list,begin,end):
+    pivot = begin
+    list[pivot],list[end-1] = list[end-1],list[pivot]
+    l = begin
+    for i in range(begin,end-1):
+        if list[i] < list[end-1]:
+            list[l],list[i] = list[i],list[l]
+            l = l+1
+    list[l],list[end-1] = list[end-1],list[l]
+    return l
 """
 Importing the testing code after function defs to ensure same references.
 """
@@ -83,6 +135,10 @@ if __name__ == "__main__":
     print()
     testingSuite(QuickSort)
     print()
-    print('DEFAULT measureTime')
+    print('UNSORTED measureTime')
     print()
     measureTime()
+    print()
+    print('SORTED measureTime')
+    print()
+    measureTime(True)
--- Project1/project1tests.py
+++ Project1/project1tests.py
@@ -1,7 +1,7 @@
 """
 Math 560
 Project 1
-Fall 2020
+Fall 2021
 Provided Testing Code
 """
 
@@ -34,8 +34,6 @@ sortedList:  the supposedly sorted list
 OUTPUTS
 returns true or false
 """
-
-
 def isSorted(unsortedList, sortedList):
     # Copy the unsorted list.
     temp = unsortedList.copy()
@@ -45,7 +43,6 @@ def isSorted(unsortedList, sortedList):
 
     # Check equality.
     return temp == sortedList
-
 
 """
 testingSuite
@@ -67,8 +64,6 @@ alg: function pointer for alg to test, the options are:
 OUTPUTS
 Printed statements indicating which tests passed/failed.
 """
-
-
 def testingSuite(alg):
     # First, we seed the random number generator to ensure reproducibility.
     random.seed(1)
@@ -80,7 +75,7 @@ def testingSuite(alg):
     # Make sure the input is a proper alg to consider.
     if not alg.__name__ in algs:
         raise Exception( \
-            'Not an allowed algorithm. Value was: {}'.format(alg.__name__))
+                'Not an allowed algorithm. Value was: {}'.format(alg.__name__))
 
     # Create a list to store all the tests.
     tests = []
@@ -93,44 +88,44 @@ def testingSuite(alg):
     message.append('singleton array')
 
     # Test 2: repeated elements
-    tests.append([1, 2, 3, 4, 5, 5, 4, 3, 2, 1])
+    tests.append([1,2,3,4,5,5,4,3,2,1])
     message.append('repeated elements')
 
     # Test 3: all repeated elements
-    tests.append([2, 2, 2, 2, 2, 2, 2, 2, 2, 2])
+    tests.append([2,2,2,2,2,2,2,2,2,2])
     message.append('all repeated elements')
 
     # Test 4: descending order
-    tests.append([10, 9, 8, 7, 6, 5, 4, 3, 2, 1])
+    tests.append([10,9,8,7,6,5,4,3,2,1])
     message.append('descending order')
 
     # Test 5: sorted input
-    tests.append([1, 2, 3, 4, 5, 6, 7, 8, 9, 10])
+    tests.append([1,2,3,4,5,6,7,8,9,10])
     message.append('sorted input')
 
     # Test 6: negative inputs
-    tests.append([-1, -2, -3, -4, -5, -5, -4, -3, -2, -1])
+    tests.append([-1,-2,-3,-4,-5,-5,-4,-3,-2,-1])
     message.append('negative inputs')
 
     # Test 7: mixed positive/negative
-    tests.append([1, 2, 3, 4, 5, -1, -2, -3, -4, -5, 0])
+    tests.append([1,2,3,4,5,-1,-2,-3,-4,-5,0])
     message.append('mixed positive/negative')
 
     # Test 8: array of size 2^k - 1
-    temp = list(range(0, 2 ** 6 - 1))
+    temp = list(range(0,2**6-1))
     random.shuffle(temp)
     tests.append(temp)
     message.append('array of size 2^k - 1')
 
     # Test 9: random real numbers
-    tests.append([random.random() for x in range(0, 2 ** 6 - 1)])
+    tests.append([random.random() for x in range(0,2**6-1)])
     message.append('random real numbers')
 
     # Store total number of passed tests.
     passed = 0
 
     # Loop over the tests.
-    for tInd in range(0, len(tests)):
+    for tInd in range(0,len(tests)):
         # Copy the test for sorting.
         temp = tests[tInd].copy()
 
@@ -141,16 +136,16 @@ def testingSuite(alg):
 
             # Check if the test succeeded.
             if isSorted(temp, tests[tInd]):
-                print('Test %d Success: %s' % (tInd + 1, message[tInd]))
+                print('Test %d Success: %s' % (tInd+1, message[tInd]))
                 passed += 1
             else:
-                print('Test %d FAILED: %s' % (tInd + 1, message[tInd]))
+                print('Test %d FAILED: %s' % (tInd+1, message[tInd]))
 
         # Catch any errors.
         except Exception as e:
             print()
             print('DANGER!')
-            print('Test %d threw an error: %s' % (tInd + 1, message[tInd]))
+            print('Test %d threw an error: %s' % (tInd+1, message[tInd]))
             print('Error: ')
             print(e)
             print()
@@ -159,7 +154,6 @@ def testingSuite(alg):
     print()
     print('%d/9 Tests Passed' % passed)
     return
-
 
 """
 measureTime
@@ -180,9 +174,7 @@ OUTPUTS
 A number of genereated runtime vs n plot, a log-log plot for several
 algorithms, and printed statistics about the slope of the log-log plots.
 """
-
-
-def measureTime(preSorted=False, numTrials=30):
+def measureTime(preSorted = False, numTrials = 30):
     # Print whether we are using sorted inputs.
     if preSorted:
         print('Timing algorithms using only sorted data.')
@@ -194,17 +186,24 @@ def measureTime(preSorted=False, numTrials=30):
     # First, we seed the random number generator to ensure consistency.
     random.seed(1)
 
+    # Define a scaling constant for n for insertion and bubble sort.
+    scaleN = 1
+    ISind = 1
+    BBind = 2
+    TIMind = 5
+
     # We now define the range of n values to consider.
     if preSorted:
         # Need to look at larger n to get a good sense of runtime.
         # Look at n from 20 to 980.
-        # Note that 1000 causes issues with recursion depth...
-        N = list(range(1, 50))
-        N = [20 * x for x in N]
+        # Note that 1000 causes issues with recursion depth for merge and quick.
+        N = list(range(1,50))
+        N = [20*x for x in N]
+        scaleN = 10 # Make insertion and bubble do larger tests.
     else:
         # Look at n from 10 to 500.
-        N = list(range(1, 51))
-        N = [10 * x for x in N]
+        N = list(range(1,51))
+        N = [10*x for x in N]
 
     # Store the different algs to consider.
     algs = [SelectionSort, InsertionSort, \
@@ -223,25 +222,28 @@ def measureTime(preSorted=False, numTrials=30):
     isCorrect = [True, True, True, True, True, True]
 
     # Loop over the different sizes.
-    for nInd in range(0, len(N)):
+    for nInd in range(0,len(N)):
         # Get the current value of n to consider.
         n = N[nInd]
 
         # Reset the running sum of the runtimes.
-        timing = [0, 0, 0, 0, 0, 0]
+        timing = [0,0,0,0,0,0]
 
-        # Loop over the 30 tests.
-        for test in range(1, numTrials + 1):
-            # Create the random list of size n to sort.
-            listToSort = list(range(0, n))
-            listToSort = [random.random() for x in listToSort]
-
-            if preSorted:
-                # Pre-sort the list.
-                listToSort.sort()
+        # Loop over the tests.
+        for test in range(1,numTrials+1):
 
             # Loop over the algs.
-            for aI in range(0, len(algs)):
+            for aI in range(0,len(algs)):
+                # If preSorted and insertion or bubble or tim, make the
+                # testing list longer to get better tests...
+                if preSorted and (aI == ISind or aI == BBind or aI == TIMind):
+                    listToSort = list(range(0,n*scaleN))
+                else:
+                    listToSort = list(range(0,n))
+
+                    if not preSorted:
+                        listToSort = [random.random() for x in listToSort]
+
                 # Grab the name of the alg.
                 alg = algs[aI]
 
@@ -250,21 +252,21 @@ def measureTime(preSorted=False, numTrials=30):
 
                 # Time the sort.
                 t = time.time()
-                if aI != 4:
+                if aI != 4 :
                     alg(copiedList)
                 else:
-                    alg(copiedList, 0, len(copiedList))
+                    alg(copiedList,0,len(copiedList))
                 t = time.time() - t
 
                 # Ensure that your function sorted the list.
-                if not isSorted(listToSort, copiedList):
+                if not isSorted(listToSort,copiedList):
                     isCorrect[aI] = False
 
                 # Add the time to our running sum.
                 timing[aI] += t
 
         # Now that we have completed the numTrials tests, average the times.
-        timing = [x / numTrials for x in timing]
+        timing = [x/numTrials for x in timing]
 
         # Store the times for this value of n.
         tSelectionSort[nInd] = timing[0]
@@ -275,18 +277,19 @@ def measureTime(preSorted=False, numTrials=30):
         tPython[nInd] = timing[5]
 
     # If there was an error in one of the plotting algs, report it.
-    for aI in range(0, len(algs) - 1):
+    for aI in range(0,len(algs)-1):
         if not isCorrect[aI]:
             print('%s not implemented properly!!!' % algs[aI].__name__)
 
     # Now plot the timing data.
-    for aI in range(0, len(algs)):
+    for aI in range(0,len(algs)):
         # Get the alg.
         alg = algs[aI].__name__ if aI != 5 else 'Python'
 
         # Plot.
+        scaleNP = scaleN if (aI == ISind or aI == BBind or aI == TIMind) else 1
         plt.figure()
-        plt.plot(N, locals()['t%s' % alg])
+        plt.plot([scaleNP*nn for nn in N],locals()['t%s' % alg])
         plt.title('%s runtime versus n' % alg)
         plt.xlabel('Input Size n')
         plt.ylabel('Runtime (s)')
@@ -298,13 +301,16 @@ def measureTime(preSorted=False, numTrials=30):
     # Plot them all together.
     plt.figure()
     fig, ax = plt.subplots()
-    ax.plot(N, tSelectionSort, label='Selection')
-    ax.plot(N, tInsertionSort, label='Insertion')
-    ax.plot(N, tBubbleSort, label='Bubble')
-    ax.plot(N, tMergeSort, label='Merge')
-    ax.plot(N, tQuickSort, label='Quick')
-    ax.plot(N, tPython, label='Python')
-    legend = ax.legend(loc='upper left')
+    ax.plot(N,tSelectionSort, label='Selection')
+    ax.plot([scaleN*nn for nn in N],tInsertionSort, label='Insertion')
+    ax.plot([scaleN*nn for nn in N],tBubbleSort, label='Bubble')
+    ax.plot(N,tMergeSort, label='Merge')
+    ax.plot(N,tQuickSort, label='Quick')
+    ax.plot([scaleN*nn for nn in N],tPython, label='Python')
+    if preSorted:
+        legend = ax.legend(loc='upper right')
+    else:
+        legend = ax.legend(loc='upper left')
     plt.title('All sorting runtimes versus n')
     plt.xlabel('Input Size n')
     plt.ylabel('Runtime (s)')
@@ -314,24 +320,26 @@ def measureTime(preSorted=False, numTrials=30):
         plt.savefig('sorting.png', bbox_inches='tight')
 
     # Now look at the log of the sort times.
-    logN = [(numpy.log(x) if x > 0 else -16) for x in N]
-    logSS = [(numpy.log(x) if x > 0 else -16) for x in tSelectionSort]
-    logIS = [(numpy.log(x) if x > 0 else -16) for x in tInsertionSort]
-    logBS = [(numpy.log(x) if x > 0 else -16) for x in tBubbleSort]
-    logMS = [(numpy.log(x) if x > 0 else -16) for x in tMergeSort]
-    logQS = [(numpy.log(x) if x > 0 else -16) for x in tQuickSort]
+    logN = [(numpy.log(x) if x>0 else -16) for x in N]
+    scaleLogN = [(numpy.log(x) if x>0 else -16) \
+                 for x in [scaleN*nn for nn in N]]
+    logSS = [(numpy.log(x) if x>0 else -16) for x in tSelectionSort]
+    logIS = [(numpy.log(x) if x>0 else -16) for x in tInsertionSort]
+    logBS = [(numpy.log(x) if x>0 else -16) for x in tBubbleSort]
+    logMS = [(numpy.log(x) if x>0 else -16) for x in tMergeSort]
+    logQS = [(numpy.log(x) if x>0 else -16) for x in tQuickSort]
 
     # Linear regression.
-    mSS, _, _, _, _ = stats.linregress(logN, logSS)
-    mIS, _, _, _, _ = stats.linregress(logN, logIS)
-    mBS, _, _, _, _ = stats.linregress(logN, logBS)
+    mSS, _, _, _, _ = stats.linregress(logN,logSS)
+    mIS, _, _, _, _ = stats.linregress(scaleLogN,logIS)
+    mBS, _, _, _, _ = stats.linregress(scaleLogN,logBS)
 
     # Plot log-log figure.
     plt.figure()
     fig, ax = plt.subplots()
-    ax.plot(logN, logSS, label='Selection')
-    ax.plot(logN, logIS, label='Insertion')
-    ax.plot(logN, logBS, label='Bubble')
+    ax.plot(logN,logSS, label='Selection')
+    ax.plot(scaleLogN,logIS, label='Insertion')
+    ax.plot(scaleLogN,logBS, label='Bubble')
     legend = ax.legend(loc='upper left')
     plt.title('Log-Log plot of runtimes versus n')
     plt.xlabel('log(n)')
@@ -348,32 +356,35 @@ def measureTime(preSorted=False, numTrials=30):
     print('Bubble Sort log-log Slope (all n): %f' % mBS)
     print()
 
-    # Now strip off all n<200...
-    logN = logN[19:]
-    logSS = logSS[19:]
-    logIS = logIS[19:]
-    logBS = logBS[19:]
-    logMS = logMS[19:]
-    logQS = logQS[19:]
+    # Now strip off all n<200 (or 400 for preSorted)...
+    cutoff = 400 if preSorted else 200
+    indCut = (cutoff//20)-1 if preSorted else (cutoff//10)-1
+    logN = logN[indCut:]
+    scaleLogN = scaleLogN[indCut:]
+    logSS = logSS[indCut:]
+    logIS = logIS[indCut:]
+    logBS = logBS[indCut:]
+    logMS = logMS[indCut:]
+    logQS = logQS[indCut:]
 
     # Linear regression.
-    mSS, _, _, _, _ = stats.linregress(logN, logSS)
-    mIS, _, _, _, _ = stats.linregress(logN, logIS)
-    mBS, _, _, _, _ = stats.linregress(logN, logBS)
-    mMS, _, _, _, _ = stats.linregress(logN, logMS)
-    mQS, _, _, _, _ = stats.linregress(logN, logQS)
+    mSS, _, _, _, _ = stats.linregress(logN,logSS)
+    mIS, _, _, _, _ = stats.linregress(scaleLogN,logIS)
+    mBS, _, _, _, _ = stats.linregress(scaleLogN,logBS)
+    mMS, _, _, _, _ = stats.linregress(logN,logMS)
+    mQS, _, _, _, _ = stats.linregress(logN,logQS)
 
     # Print the regression info.
     print('Selection Sort log-log Slope (n>%d): %f' \
-          % (400 if preSorted else 200, mSS))
+          % (cutoff, mSS))
     print('Insertion Sort log-log Slope (n>%d): %f' \
-          % (400 if preSorted else 200, mIS))
+          % (cutoff, mIS))
     print('Bubble Sort log-log Slope (n>%d): %f' \
-          % (400 if preSorted else 200, mBS))
+          % (cutoff, mBS))
     print('Merge Sort log-log Slope (n>%d): %f' \
-          % (400 if preSorted else 200, mMS))
+          % (cutoff, mMS))
     print('Quick Sort log-log Slope (n>%d): %f' \
-          % (400 if preSorted else 200, mQS))
+          % (cutoff, mQS))
 
     # Close all figures.
     plt.close('all')
