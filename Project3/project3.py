b93dbecf951631907c7130386a1f6772770ea8ec
Julius-1998 <89859253+Julius-1998@users.noreply.github.com>
2021-11-06 15:29:34 -0400

    Project3 commented

--- Project3/project3.py
+++ Project3/project3.py
@@ -24,21 +24,29 @@ def detectArbitrage(adjList, adjMat, tol=1e-15):
         u.dist = math.inf
         u.prev = None
     adjList[0].dist = 0
+    #Bellman-Ford Algorithm
+    #Go N-1 rounds
     for i in range(0,len(adjMat[0])-1):
         for u in adjList:
             for v in u.neigh:
                 if v.dist > u.dist + adjMat[u.rank][v.rank] + tol:
                     v.dist = u.dist + adjMat[u.rank][v.rank]
                     v.prev = u
+    #One extra round to determine negative circle
     for u in adjList:
         for v in u.neigh:
             if v.dist > u.dist + adjMat[u.rank][v.rank] + tol:
                 temp = v
                 v.prev = u
+                #to get the negative loop, reverse the loop
                 ans.insert(0,temp.rank)
-                while temp.prev!=v:
+                while temp.prev.rank not in ans:
                     temp = temp.prev
                     ans.insert(0,temp.rank)
+                #trim the unnecessary nodes
+                index = ans.index(temp.prev.rank)
+                for i in range(index+1,len(ans)):
+                    del ans[index+1]
                 ans.insert(0,temp.prev.rank)
                 return ans
 
